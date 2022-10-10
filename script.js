eb6fd7ff7377c184827e3caae47bd41f31dcab9a
BatorGG <bator.karpathegyi@gmail.com>
2022-10-10 21:52:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var cover = document.getElementsByClassName("cover")[0];
+var videodiv = document.getElementsByClassName("videodiv")[0];
+var visible = document.getElementsByClassName("visible")[0];
+cover.addEventListener("click", () => {
+   // cover.style.display = "none";
+    cover.classList.toggle("fade");
+    setTimeout(() => {
+        cover.style.display = "none";
+        visible.style.display = "block";
+    }, 1000);
+
+    console.log("clicked");
+    videodiv.innerHTML = '<video autoplay loop playsinline preload="auto" class="video" id="video"> <source src="./video.mp4" type="video/mp4"> </video>';
+    
+});
