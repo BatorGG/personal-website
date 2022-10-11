969c622134cda0683c4f20cdf17064dc9e16c41c
BatorGG <bator.karpathegyi@gmail.com>
2022-10-11 10:19:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ var videodiv = document.getElementsByClassName("videodiv")[0];
 var visible = document.getElementsByClassName("visible")[0];
 cover.addEventListener("click", () => {
    // cover.style.display = "none";
-    cover.classList.toggle("fade");
+   cover.children[0].style.display = "none";
+   cover.children[1].style.display = "none";
+
+    
     setTimeout(() => {
+        cover.classList.toggle("fade");
+        setTimeout(() => {
         cover.style.display = "none";
         visible.style.display = "block";
+        }, 1000);
     }, 1000);
 
     console.log("clicked");
